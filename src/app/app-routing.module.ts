4f3bdbfc94bf784fdc64975fe23c71c80f8cde9e
HECTOR DANIE VELAZQUEZ LOPEZ <hector.velazquez@elektra.com.mx>
2023-06-04 20:53:19 -0600

    inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './views/inicio/inicio.component';
+import { AccesibilidadComponent } from './views/accesibilidad/accesibilidad.component';
+import { ContactoComponent } from './views/contacto/contacto.component';
+import { InstalacionesComponent } from './views/instalaciones/instalaciones.component';
+import { CursosComponent } from './views/cursos/cursos.component';
+import { QuienesSomosComponent } from './views/quienes-somos/quienes-somos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'accesibilidad', component: AccesibilidadComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'instalaciones', component: InstalacionesComponent },
+  { path: 'cursos', component: CursosComponent },
+  { path: 'about', component: QuienesSomosComponent },
+  { path: '**', redirectTo: 'inicio', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './views/inicio/inicio.component';
+import { AccesibilidadComponent } from './views/accesibilidad/accesibilidad.component';
+import { QuienesSomosComponent } from './views/quienes-somos/quienes-somos.component';
+import { ContactoComponent } from './views/contacto/contacto.component';
+import { CursosComponent } from './views/cursos/cursos.component';
+import { InstalacionesComponent } from './views/instalaciones/instalaciones.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AccesibilidadComponent,
+    QuienesSomosComponent,
+    ContactoComponent,
+    CursosComponent,
+    InstalacionesComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
